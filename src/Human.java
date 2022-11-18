2edaeba81a1515abedf233e4b8e76876dd342c77
ShuEugene <shueugene@hotmail.com>
2022-11-18 12:22:20 +0300

    C2.L2.HW1.1 is completed.

--- src/Human.java
+++ src/Human.java
@@ -1,58 +1,131 @@
+import java.time.LocalDate;
+
 public class Human {
 
-    String name, town, jobTitle;
-    int yearOfBirth;
+    private String name, town, jobTitle;
+    private int yearOfBirth;
 
-    static final String UNKNOWN_INFO = "Информация не указана";
+    static final String UNKNOWN_INFO = "\"Информация не указана\"";
+
+    enum TaleSubjects {NAME, TOWN, YEAR_OF_BIRTH, JOB_TITLE;}
+
+    Human(String name, String town, int yearOfBirth, String jobTitle) {
+        setName(name);
+        setTown(town);
+        setYearOfBirth(yearOfBirth);
+        setJobTitle(jobTitle);
+    }
 
     Human(String name, String town, int yearOfBirth) {
-        this.name = name;
-        this.town = town;
-        if (yearOfBirth > 0) {
-            this.yearOfBirth = yearOfBirth;
-        } else {
-            System.out.println("Значение возраста не может быть отрицательным.\nНа данный момент присвоено значение \"0\".");
-            this.yearOfBirth = 0;
-        }
+        this(name, town, yearOfBirth, null);
     }
 
     Human(String name, String town) {
-        this(name, town, 0);
+        this(name, town, 0, null);
     }
 
     Human(String name, int yearOfBirth) {
-        this(name, null, yearOfBirth);
+        this(name, null, yearOfBirth, null);
     }
 
     Human(String name) {
-        this(name, null, 0);
+        this(name, null, 0, null);
+    }
+
+    boolean isCorrect(String value) {
+        return value != null && !value.isBlank() && !value.isEmpty();
+    }
+
+    boolean isCorrect(int value) {
+        return value > 0;
     }
 
     public String getName() {
-        if (name == null) name = UNKNOWN_INFO;
+        if (!isCorrect(name)) return UNKNOWN_INFO;
         return name;
     }
 
+    public void setName(String name) {
+        if (isCorrect(name)) {
+            this.name = name;
+        }
+    }
+
     public String getTown() {
-        if (town == null) town = UNKNOWN_INFO;
+        if (!isCorrect(town)) return UNKNOWN_INFO;
         return town;
     }
 
+    public void setTown(String town) {
+        if (isCorrect(town)) {
+            this.town = town;
+        }
+    }
+
+    public int getYearOfBirth() {
+        if (!isCorrect(yearOfBirth)) {
+            return Math.abs(yearOfBirth);
+        }
+        return yearOfBirth;
+    }
+
+    public String getStrYearOfBirth() {
+        if (!isCorrect(yearOfBirth)) {
+            return UNKNOWN_INFO;
+        }
+        return Integer.toString(yearOfBirth);
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (!isCorrect(yearOfBirth)) {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+            return;
+        }
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setAge(int age) {
+        if (isCorrect(age)) {
+            setYearOfBirth(LocalDate.now().getYear() - age);
+        }
+    }
+
     public String getJobTitle() {
-        if (jobTitle == null) jobTitle = UNKNOWN_INFO;
+        if (!isCorrect(jobTitle)) return UNKNOWN_INFO;
         return jobTitle;
     }
 
-    void sayHello() {
-        String name, town, jobTitle;
-        name = getName();
-        town = getTown();
-        jobTitle = getJobTitle();
-        if (name.equals(UNKNOWN_INFO) || town.equals(UNKNOWN_INFO) || jobTitle.equals(UNKNOWN_INFO) || yearOfBirth == 0) {
-            System.out.println("\n! Часть сведений о человеке отсутствует. Знакомство исключено.");
-            return;
+    public void setJobTitle(String jobTitle) {
+        if (isCorrect(jobTitle)) {
+            this.jobTitle = jobTitle;
         }
-        System.out.printf("%nПривет!%nМеня зовут %s. Я из города %s. Я родился(-лась) в %d году." +
-                "%nЯ работаю в должности «%s».%nБудем знакомы!%n", name, town, yearOfBirth, jobTitle);
+    }
+
+    String pronounce(TaleSubjects taleSubjects) {
+        String sentence;
+        switch (taleSubjects) {
+            case NAME:
+                sentence = isCorrect(name) ? String.format("Меня зовут %s.", name) : "Я забыл(-а), как меня зовут.";
+                break;
+            case TOWN:
+                sentence = isCorrect(town) ? String.format("Я из города %s.", town) : "Не помню, откуда я.";
+                break;
+            case YEAR_OF_BIRTH:
+                sentence = isCorrect(yearOfBirth) ? String.format("Родился(-лась) в %d году.", yearOfBirth) : "Когда родился(-лась) - не помню.";
+                break;
+            case JOB_TITLE:
+                sentence = isCorrect(jobTitle) ? String.format("Работаю в должности «%s».", jobTitle) : "Пока что не работаю.";
+                break;
+            default:
+                sentence = "Об этом мне нечего рассказать.";
+        }
+        return sentence;
+    }
+
+    void tellAboutYourself() {
+        System.out.println("\nПривет!");
+        System.out.printf("%s %s %s\n%s\n", pronounce(TaleSubjects.NAME), pronounce(TaleSubjects.TOWN),
+                pronounce(TaleSubjects.YEAR_OF_BIRTH), pronounce(TaleSubjects.JOB_TITLE));
+        System.out.println("Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,29 @@
+
+//  Домашние задания 2-го курса
+
 public class Main {
     public static void main(String[] args) {
 
-//        ДЗ 1.1, 1.2, 2.1, 3.1
+//        У1.ДЗ: 1.1, 1.2, 2.1, 3.1; У2.ДЗ: 1.1
         Human maxim = new Human("Максим", "Минск", 1988);
         Human anna = new Human("Аня", "Москва", 1993);
         Human katia = new Human("Катя", 1992);
         Human artiom = new Human("Артём", "Москва", 1995);
+        Human vladimir = new Human("Владимир", "Казань");
+        vladimir.setAge(21);
 
-        maxim.jobTitle = "Бренд-менеджер";
-        anna.jobTitle = "Методист образовательных программ";
-        katia.jobTitle = "Продукт-менеджер";
-        artiom.jobTitle = "Директор по развитию бизнеса";
+        maxim.setJobTitle("Бренд-менеджер");
+        anna.setJobTitle("Методист образовательных программ");
+        katia.setJobTitle("Продукт-менеджер");
+        artiom.setJobTitle("Директор по развитию бизнеса");
 
-        maxim.sayHello();
-        anna.sayHello();
-        katia.sayHello();
-        artiom.sayHello();
+        maxim.tellAboutYourself();
+        anna.tellAboutYourself();
+        katia.tellAboutYourself();
+        artiom.tellAboutYourself();
+        vladimir.tellAboutYourself();
 
-//        ДЗ 1.3, 2.2. 3.2
+//        У1.ДЗ: 1.3, 2.2. 3.2
         Car ladaGranta = new Car("Лада", "Гранта", "жёлтый", "Россия", 2015, 1.7f);
         Car audiA8_50L = new Car("Audi", "A8 50 L TDI quattro", "чёрный", "Германия", 2020, 3.0f);
         Car bmwZ8 = new Car("BMW", "Z8", "чёрный", "Германия", 2021, 3.0f);
